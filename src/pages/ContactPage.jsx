6a22c6c4878ea05d50f105f63546dfa843982883
timon.foerster <t.j.foerster@web.de>
2022-02-10 00:10:40 +0100

    use material ui and ts

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -14,15 +14,15 @@ function ContactPage() {
                     <h2 style={{ margin:0 }}>Contact me</h2>
                 </div>
                 <div data-swiper-parallax="-200" data-swiper-parallax-opacity="0.5" className="m-3">
-                    <a href="mailto:timon.dienst@webfoerster.de" class="d-flex gap-3 align-items-center contact-link">
+                    <a href="mailto:&#116;&#105;&#109;&#111;&#110;&#46;&#100;&#105;&#101;&#110;&#115;&#116;&#64;&#119;&#101;&#98;&#102;&#111;&#101;&#114;&#115;&#116;&#101;&#114;&#46;&#100;&#101;" class="d-flex gap-3 align-items-center contact-link">
                         <FontAwesomeIcon icon={['fas', 'envelope']} size="5x" id="email-icon" />
-                        <span id="email">timon.dienst(at)webfoerster.de</span>
+                        <span id="email">&#116;&#105;&#109;&#111;&#110;&#46;&#100;&#105;&#101;&#110;&#115;&#116;&#64;&#119;&#101;&#98;&#102;&#111;&#101;&#114;&#115;&#116;&#101;&#114;&#46;&#100;&#101;</span>
                     </a>
                 </div>
                 <div data-swiper-parallax="-100" data-swiper-parallax-opacity="0.5" className="m-3">
                     <a href="https://github.com/tjfoerster" target="_blank" rel="noopener noreferrer" class="d-flex gap-3 align-items-center contact-link">
                         <FontAwesomeIcon icon={['fab', 'github']} size="5x" id="github-icon" />
-                        <span id="github">github.com</span>
+                        <span id="github">github.com/tjfoerster</span>
                     </a>
                 </div>
             </div>

7389aac7aa66c9eb151c890b40798bd5c0f7182f
timon.foerster <t.j.foerster@web.de>
2022-02-12 17:39:41 +0100

    ui fix

--- src/pages/FunfactPage.jsx
+++ src/pages/FunfactPage.jsx
@@ -15,7 +15,7 @@ function FunfactPage() {
                     <p>A random fun fact to make the day a little better ;-)</p>
                 </div>
                 <div data-swiper-parallax="-200" data-swiper-parallax-opacity="0.5">
-                    <a href="https://de.m.wikipedia.org/wiki/Fu%C3%9Fballl%C3%A4nderspiel_Barbados_%E2%80%93_Grenada_1994" target="_blank" rel="noopener noreferrer" className='fun-link' style={{ color: theme.palette.text.primary}}>
+                    <a href="https://de.m.wikipedia.org/wiki/Fu%C3%9Fballl%C3%A4nderspiel_Barbados_%E2%80%93_Grenada_1994" target="_blank" rel="noopener noreferrer" className='fun-link' style={{ color: theme.palette.text.primary }}>
                         <FontAwesomeIcon icon={['fas', 'external-link-alt']} size="5x" id="link-icon" />
                         <span id="link">https://de.m.wikipedia.org/wiki/Fu%C3%9Fballl%C3%A4nderspiel_Barbados_%E2%80%93_Grenada_1994</span>
                     </a>
